a4b149786779f2f8cb26b957e6c7a9783fdf301e
Pravin Nawale <159033956+PravinNawale7@users.noreply.github.com>
2024-09-21 08:50:05 +0530

    Spring Boot Demo
    In this program demonstrate the Spring Boot Frist Project

---
+++ SpringBootDemo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String disp(Model model) {
+		return "index";
+		
+	}
+
+}
